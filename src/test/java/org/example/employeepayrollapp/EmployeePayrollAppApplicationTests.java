b90f70efba4b924acc5216be949b67c3cf46c950
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-05 23:14:48 +0530

    [Falgun Patel] Initialise the EmployeePayrollApp Repository
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

---
+++ src/test/java/org/example/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
